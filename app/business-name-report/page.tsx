6aefdbc9581cfe9a2282219d9c6cc26759eb4103
chiraganantasecuritylab123 <chirag@anantasecuritylab.com>
2026-06-24 15:11:46 +0530

    page no 3 complated `ExecutiveSummary`

--- app/business-name-report/page.tsx
+++ app/business-name-report/page.tsx
@@ -1,4 +1,6 @@
 import BusinessReportPage from "@/Components/BusinessNameReport/BusinessReportPage";
+import ExecutiveSummary from "@/Components/BusinessNameReport/ExecutiveSummary";
+import PartnerDetails from "@/Components/BusinessNameReport/PartnerDetails";
 import ReportHomeClient from "@/Components/ReportHomeClient";
 
 export default function BusinessNameReportPage() {
@@ -8,7 +10,9 @@ export default function BusinessNameReportPage() {
       reportDescription="Business name vibration and analysis"
     >
 
-      <BusinessReportPage  /> {/* 1 */}
+      <BusinessReportPage /> {/* 1 */}
+      <ExecutiveSummary /> {/* 3 — Executive Summary */}
+      {/* <PartnerDetails pageNumber="03" /> */}
 
     </ReportHomeClient>
   );
